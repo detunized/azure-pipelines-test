4140305515f15f8dd2f2f7e72dc199809ef15ff7
Dmitry Yakimenko <detunized@gmail.com>
2019-11-13 22:01:56 +0100

    Test app/lib

---
+++ app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using lib;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new lib.Class1();
+            Console.WriteLine($"Hello World! {c}");
+        }
+    }
+}
